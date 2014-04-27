0d351450337980061365bf0f3c414498c048f753
Johannes Meijer van Putten <johannes@falskdansker.net>
2014-04-27 13:12:44 +0200

    Don't use static page titles

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Pigeon keeper</title>
+    <title><?= $sitename ?></title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
